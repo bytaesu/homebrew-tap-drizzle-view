9d435e9dbe1d89e7d455f99093e67e20a1b7ad2a
Taesu <bytaesu@gmail.com>
2025-09-07 19:26:18 +0900

    update sha256

--- Formula/drizzle-view.rb
+++ Formula/drizzle-view.rb
@@ -7,14 +7,14 @@ class DrizzleView < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/bytaesu/drizzle-view/releases/download/v#{version}/drizzle-view-darwin-amd64.tar.gz"
-      sha256 "**********" # TODO: update
+      sha256 "fe427f3a44c198c9a8b71cee62a89fad7413cebc68f26acff82b2738fb76ad45"
 
       def install
         bin.install "drizzle-view-darwin-amd64" => "drizzle-view"
       end
     elsif Hardware::CPU.arm?
       url "https://github.com/bytaesu/drizzle-view/releases/download/v#{version}/drizzle-view-darwin-arm64.tar.gz"
-      sha256 "**********" # TODO: update
+      sha256 "fe427f3a44c198c9a8b71cee62a89fad7413cebc68f26acff82b2738fb76ad45"
 
       def install
         bin.install "drizzle-view-darwin-arm64" => "drizzle-view"
