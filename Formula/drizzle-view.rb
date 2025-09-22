6fb9543946b7884477037c04d1f16bab5cfe05a5
Taesu <bytaesu@gmail.com>
2025-09-22 19:42:24 +0900

    adjust sha256

--- Formula/drizzle-view.rb
+++ Formula/drizzle-view.rb
@@ -1,16 +1,16 @@
 class DrizzleView < Formula
   desc "Unified interface for Drizzle Studio and Drizzle Visualizer"
   homepage "https://github.com/bytaesu/drizzle-view"
-  version "0.0.2"
+  version "0.0.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/bytaesu/drizzle-view/releases/download/v0.0.2/drizzle-view-darwin-x64.tar.gz"
-      sha256 "374518e0bb1f3f6ac0a5a78a70dc6e11911e69ca93941ff3f4f60a6876b3f3c6"
+      url "https://github.com/bytaesu/drizzle-view/releases/download/v#{version}/drizzle-view-darwin-x64.tar.gz"
+      sha256 "82c8eb6b09966c844537a33d04ccfd5b22187cb6f27ce00c053fb113ba86b784"
 
       def install
-        bin.install "drizzle-view-darwin-amd64" => "drizzle-view"
+        bin.install "drizzle-view-darwin-x64" => "drizzle-view"
       end
     elsif Hardware::CPU.arm?
       url "https://github.com/bytaesu/drizzle-view/releases/download/v#{version}/drizzle-view-darwin-arm64.tar.gz"
